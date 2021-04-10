74dc70cdf1ea42780534b1a133c5dceea28369f9
Stephan Kern <stephan@swmmrman.com>
2021-04-10 02:57:45 -0600

    Fix mode and remove -e from echo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ if [ `whoami` != "root" ] || [ $UID == 0 ]; then
   exit 1;
 fi
 apt-get install python3-smbus i2c-tools python3-rpi.gpio
-echo -e 'i2c-dev' >> /etc/modules
+echo 'i2c-dev' >> /etc/modules
 modprobe -a i2c-dev
 echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 sed -i '$ i echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device' /etc/rc.local
